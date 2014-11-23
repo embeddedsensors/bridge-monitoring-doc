f5ffd57c9841a38fa0320e6180844fa41160ecbb
lancer6x <moranodylan@gmail.com>
2014-11-23 18:06:00 -0500

    end point PZT data

--- Free Hanging Tests/Experiment 2/R114_20ft/Fourier Analysis/fourier.m
+++ Free Hanging Tests/Experiment 2/R114_20ft/Fourier Analysis/fourier.m
@@ -1,11 +1,13 @@
 clc; close all;
-load R114_data.mat
+% load R114_data.mat
 
-fs_sound = 44100;
-
+fs_sound = fs;
+%  note, higher frequencies deminish when tested 
+%  from the far end point and when using rubber. 
+% 424 Hz still fully developed. 
 % Calculate time domain for contact mic and plot
-i = 164000:1097000;
-sound = metal_axial_piezo_midpoint;
+i = 5.73e5:8.637e5;
+sound = data;
 sound_length_s = length(sound)/fs_sound;
 sound_domain = linspace(0,sound_length_s,length(sound));
 soundsample = sound(i);
--- Free Hanging Tests/Experiment 2/R114_20ft/Modal Analysis/frequencymodes.m
+++ Free Hanging Tests/Experiment 2/R114_20ft/Modal Analysis/frequencymodes.m
@@ -6,4 +6,10 @@ r = 0.00635; %thickness in meters
 
 frn = modefreq(L,r,n,youngs,density)
 figure()
-plot(frn,n,'*')
+plot(frn(1:8),n(1:8),'--r')
+hold on
+plot(data,n(1:8),'o')
+title('Theoretical Vs. Experimental Frequencies')
+xlabel('Frequency (Hz)')
+ylabel('Mode (n)')
+
