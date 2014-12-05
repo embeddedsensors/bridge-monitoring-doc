533e0c2c1c21270dfb3b025b430a0a22e9d2d9f3
lancer6x <moranodylan@gmail.com>
2014-12-04 19:44:54 -0500

    pzt data

--- Free Hanging Tests/Experiment 2/R114_20ft/Fourier Analysis/fourier.m
+++ Free Hanging Tests/Experiment 2/R114_20ft/Fourier Analysis/fourier.m
@@ -6,14 +6,15 @@ fs_sound = fs;
 %  from the far end point and when using rubber. 
 % 424 Hz still fully developed. 
 % Calculate time domain for contact mic and plot
-i = 5.73e5:8.637e5;
-sound = data;
+i = 1.28e5:4.837e5;
+sound = data- mean(data);
 sound_length_s = length(sound)/fs_sound;
 sound_domain = linspace(0,sound_length_s,length(sound));
 soundsample = sound(i);
 sound_time = sound_domain(i) - sound_domain(i(1));
 
 figure()
+set(gca,'fontsize', 18);
 plot(sound_time,soundsample)
 title('Sound Waveform vs Time')
 ylabel('Sound Intensity')
@@ -21,10 +22,11 @@ xlabel('Time (s)')
 
 % Frequency content of each sample
 figure()
+set(gca,'fontsize', 18);
 myfft(soundsample,fs_sound);
-title('Fourier Transform of midpoint sound sample')
+title('FFT of transverse rubber strike recorded from the midpoint')
 xlabel('Frequency')
 ylabel('Response')
-axis([0 8000 0 30000])
+% axis([0 8000 0 30000])
 
 save R114_data.mat
